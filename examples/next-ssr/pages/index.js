6b6e71dc82a0dba9df9a28f473a78ddff3a0a598
Charles King <cking@weedmaps.com>
2017-04-09 13:20:44 -0700

    docs(repo): added next.js example
    Added next.js example to show it working with SSR

---
+++ examples/next-ssr/pages/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Atom from '../components/atom';
+import Head from 'next/head';
+
+export default () => (
+  <div>
+    <Head>
+      <title>react-web-animation: Next.js</title>
+      <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,700' rel='stylesheet' type='text/css'/>
+      <style>
+        {
+          `body {
+        font-family: Roboto, Helvetica, Arial, Sans-Serif;
+        font-size: 16px;
+        font-weight: 300;
+        overflow-y: hidden;
+        margin: 0;
+        padding: 0;
+      }
+
+        button {
+        padding: 4px;
+        margin: 2px;
+        background: #fff;
+        outline: none;
+        border: 1px solid #ddd;
+        height: 25px;
+        border-radius: 3px;
+        line-height: 16px;
+      }`
+        }
+      </style>
+    </Head>
+    <Atom/>
+  </div>
+)
